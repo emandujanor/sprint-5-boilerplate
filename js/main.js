a5c3ff7a6e550ec2a4045ee13547f45f91be2fd0
Edith Mandujano <edithmandujano001@gmail.com>
2017-06-15 15:38:40 -0500

    Permite crear nuevo tema y mostrarlo

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 var api = {
   url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics/'
 };
-var cargarPagina= function(){
-  mostrarTemas();
+var contadorTemas = 0;
+var cargarPagina = function() {
   cargarFormulario();
-  cargarTemas();
+  cargarTemas(); //
 }
 
-var cargarTemas = function () {
-  $.getJSON(api.url, function (response) {
-    response.forEach(crearTemas);
+var cargarTemas = function() {
+  $.getJSON(api.url, function(response) {
+    response.forEach(mostrarTemas);
   });
 }
-var crearTemas= function(tema){
-  var contenido= tema.content;
-  var autor= tema.author_name;
-  var id= tema.id;
-  var respuestas= tema.responses_count;
-  console.log("nombre: "+ autor+ "id: " + id);
+var mostrarTemas = function(tema) {
+  var contenido = tema.content;
+  var autor = tema.author_name;
+  var id = tema.id;
+  var respuestas = tema.responses_count;
+  contadorTemas += 1;
+
+  //console.log("nombre: "+ autor+ "id: " + id);
 
   // creamos la fila
-  var $tr = $("<tr />");
+  var $tr = $("<tr class='tema_fila'></tr>");
   // creamos la celda del tema
   var $contenidoTd = $("<td />");
   $contenidoTd.text(contenido);
@@ -29,27 +31,43 @@ var crearTemas= function(tema){
   $nombreTd.text(autor);
   // creamos la celda del respuestas
   var $respuestasTd = $("<td />");
-  $respuestasTd .text(respuestas);
+  $respuestasTd.text(respuestas);
 
   // agregamos las celdas a la fila
   $tr.append($contenidoTd);
   $tr.append($nombreTd);
   $tr.append($respuestasTd);
   // agregamos filas a la tabla
-  var $tabla= $("#lista-temas");
+  var $tabla = $("#lista-temas");
   $tabla.append($tr);
 
 };
 
 
-var mostrarTemas = function (){
-  console.log(api.url);
-}
-var cargarFormulario=function (){
-  var formCrearTema=$("#formCrearTema")
+
+var cargarFormulario = function() {
+  var formCrearTema = $("#formCrearTema");
   formCrearTema.hide();
-  $("#crearTema").on("click",function(){
-  formCrearTema.show();
+  $("#crearTema").on("click", function() {
+    formCrearTema.show();
+  });
+  var $botonCrearTema = $("#nuevoTema");
+  $botonCrearTema.click(agregarTema);
+};
+
+
+var agregarTema = function(e) {
+  e.preventDefault();
+  var tema = $("#tema").val();
+  var autor = $("#autor").val();
+  var idTemaNuevo = contadorTemas + 1;
+  $.post(api.url, {
+    "id": idTemaNuevo,
+    "author_name": autor,
+    "content": tema
+  }, function(tema) {
+    mostrarTemas(tema);
+    $("#formCrearTema").hide();
   });
 };
 
